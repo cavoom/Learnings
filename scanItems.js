4e34bc128b94f57ae87a195ca0a38362739cc9e1
Dave <haasdp@gmail.com>
2022-02-20 13:19:19 -0500

    worked on scanItems in twentyOneTable

--- scanItems.js
+++ scanItems.js
@@ -14,30 +14,35 @@
     const params = {
       // Specify which items in the results are returned.
     //   FilterExpression: "Subtitle = :topic AND Season = :s AND Episode = :e",
-    FilterExpression: "campaignCode = :campaign",
+
+    // ****** COMMENTED OUT FOR TEST
+    // FilterExpression: "campaignCode = :campaign",
+    FilterExpression: "id = :theUserId",
 
       // Define the expression attribute value, which are substitutes for the values you want to compare.
       ExpressionAttributeValues: {
-     ":campaign": {S: "aaa"}
+
+    // ****** COMMENTED OUT FOR TEST
+     // ":campaign": {S: "aaa"}
+     ":theUserId" : {S: "amzn1.ask.account.AF423XPDEBEAV5LOUMXGL4FUGNH2K6W4CZLIG32UGNSCWTQOTBPSOJFVW23365CG35FLPRYACBFYELICFPBR7IRV2ST3AXW4H6MF3XJ4Q24RTBNN2B6DIOYRMYFQLDLLTF2LQMRLOSM6C7RTZKFFMFPDVSNXGG4FZ3G2IVEBL3E267DXVPGTZN24ZYB32OVZJPW3FUIYSI6P27I"}
       
   
       },
       // Set the projection expression, which are the attributes that you want.
       //ProjectionExpression: "Season, Episode, Title, Subtitle",
-      TableName: "CUSTOMER_LIST2",
+      
+      // ****** COMMENTED OUT FOR TEST
+      // TableName: "CUSTOMER_LIST2",
+      TableName: "twentyOneTable",
     };
     
     ddb.scan(params, function (err, data) {
       if (err) {
         console.log("Error", err);
       } else {
-        console.log("Success", data, '***************');
         data.Items.forEach(function (element, index, array) {
-        //   console.log(
-        //       "printing",
-        //       element.Title.S + " (" + element.Subtitle.S + ")"
-        //   );
-        console.log(element);
+          // This will print the names in the object
+        console.log(element.name.S);
         });
       }
     });
